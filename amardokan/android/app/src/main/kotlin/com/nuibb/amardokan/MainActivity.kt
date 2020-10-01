4f8ab25df8822524f63aba152b336214966c8d04
Nurul <nislam@relisource.com>
2020-10-01 06:39:43 +0600

    Initial commit

---
+++ amardokan/android/app/src/main/kotlin/com/nuibb/amardokan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nuibb.amardokan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ amardokan/lib/src/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Amar Dokan',
+          ),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.grey,
+      ),
+    );
+  }
+}
